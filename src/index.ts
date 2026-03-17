6d3de32f21f6e544041ce40d907dd42a1c07bfda
Ruedi Steinmann <ruedi.steinmann@cudos.ch>
2026-03-17 21:44:09 +0100

    ipns publishing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { noise } from "@chainsafe/libp2p-noise";
 import { yamux } from "@chainsafe/libp2p-yamux";
 import { dagJson } from "@helia/dag-json";
+import { ipns } from "@helia/ipns";
 import { bootstrap } from "@libp2p/bootstrap";
-import { circuitRelayTransport } from "@libp2p/circuit-relay-v2";
-import { identify } from "@libp2p/identify";
+import {
+  circuitRelayServer,
+  circuitRelayTransport,
+} from "@libp2p/circuit-relay-v2";
+import { identify, identifyPush } from "@libp2p/identify";
 import {
   type IdentifyResult,
   type Libp2pEvents,
@@ -22,8 +26,9 @@ import { FsBlockstore } from "blockstore-fs";
 import { FsDatastore } from "datastore-fs";
 import { createHelia } from "helia";
 import { Key } from "interface-datastore";
+import { ipnsSelector } from "ipns/selector";
+import { ipnsValidator } from "ipns/validator";
 import { createLibp2p } from "libp2p";
-import { CID } from "multiformats/cid";
 import { mdns } from "./mdns";
 
 import { autoNATv2 } from "@libp2p/autonat-v2";
@@ -32,7 +37,9 @@ import type {
   AddressManager,
   TransportManager,
 } from "@libp2p/interface-internal";
+import { keychain } from "@libp2p/keychain";
 import { multiaddr } from "@multiformats/multiaddr";
+import { CID } from "multiformats";
 
 const command = process.argv[2] as "add" | "get";
 if (command !== "add" && command !== "get") {
@@ -40,6 +47,7 @@ if (command !== "add" && command !== "get") {
   process.exit(1);
 }
 const useAmino = false;
+const useLan = true;
 const listenPort = command === "add" ? 7743 : 7744;
 
 interface MyHelperComponents {
@@ -52,7 +60,28 @@ class MyHelper implements Startable {
   start(): void | Promise<void> {
     // If a system has many network interfaces, such as when using docker, it makes little sense to announce them. Instead, wait for another peer to
     // observe us and then add the observed address, for example via identify or with the modified mdns.
-    this.components.transportManager.getAddrs = () => [];
+    try {
+      const transport = this.components.transportManager;
+      var orig = transport.getAddrs;
+      transport.getAddrs = () => {
+        return orig.call(transport).filter((addr) => {
+          const components = addr.getComponents();
+          if (
+            components.length === 2 &&
+            (components[0].name === "ip4" || components[0].name === "ip6") &&
+            components[1].name === "tcp"
+          ) {
+            return false;
+          }
+          return true;
+        });
+      };
+    } catch (e) {
+      console.error(
+        "Failed to monkey-patch transport getAddrs, observed address functionality may not work:",
+        e,
+      );
+    }
 
     setInterval(() => {
       console.log(
@@ -97,6 +126,12 @@ class MyHelper implements Startable {
   stop(): void | Promise<void> {}
 }
 
+function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 async function createNode() {
   // the blockstore is where we store the blocks that make up files
   const blockstore = new FsBlockstore("data/blocks-" + command);
@@ -126,7 +161,7 @@ async function createNode() {
     connectionEncrypters: [noise()],
     streamMuxers: [yamux()],
     peerDiscovery: [
-      mdns({ listenPort }),
+      ...(useLan ? [mdns({ listenPort })] : []),
       ...(useAmino
         ? [
             bootstrap({
@@ -142,14 +177,27 @@ async function createNode() {
     ],
     services: {
       identify: identify(),
-      lanDHT: kadDHT({
-        protocol: "/ipfs/lan/kad/1.0.0",
-        peerInfoMapper: removePublicAddressesMapper,
-        clientMode: false,
-        logPrefix: "libp2p:dht-lan",
-        datastorePrefix: "/dht-lan",
-        metricsPrefix: "libp2p_dht_lan",
-      }),
+      identifyPush: identifyPush(),
+      circuitRelay: circuitRelayServer(),
+      ping: ping(),
+      ...(useLan
+        ? {
+            lanDHT: kadDHT({
+              protocol: "/ipfs/lan/kad/1.0.0",
+              peerInfoMapper: removePublicAddressesMapper,
+              clientMode: false,
+              logPrefix: "libp2p:dht-lan",
+              datastorePrefix: "/dht-lan",
+              metricsPrefix: "libp2p_dht_lan",
+              validators: {
+                ipns: ipnsValidator,
+              },
+              selectors: {
+                ipns: ipnsSelector,
+              },
+            }),
+          }
+        : {}),
       ...(useAmino
         ? {
             aminoDHT: kadDHT({
@@ -158,12 +206,18 @@ async function createNode() {
               logPrefix: "libp2p:dht-amino",
               datastorePrefix: "/dht-amino",
               metricsPrefix: "libp2p_dht_amino",
+              validators: {
+                ipns: ipnsValidator,
+              },
+              selectors: {
+                ipns: ipnsSelector,
+              },
             }),
           }
         : {}),
-      ping: ping(),
       autoNATv2: autoNATv2(),
       myHelper: (components: MyHelperComponents) => new MyHelper(components),
+      keychain: keychain(),
     },
   });
 
@@ -175,38 +229,89 @@ async function createNode() {
 }
 
 const node = await createNode();
-console.log("Node started with Peer ID:", node.libp2p.peerId.toString());
+const libp2p = node.libp2p;
+const services = libp2p.services;
 
-node.libp2p.addEventListener("peer:discovery", (evt) => {
-  //   node.libp2p.dial(evt.detail.multiaddrs); // dial discovered peers
-  console.log(
-    "found peer: ",
-    evt.detail.id,
-    // evt.detail.multiaddrs.map((a) => a.toString()),
-  );
-});
+// services.lanDHT?.refreshRoutingTable();
+
+console.log("Node started with Peer ID:", node.libp2p.peerId.toString());
+const name = ipns(node, {});
 
 if (command === "add") {
   console.log("Adding content...");
   const j = dagJson(node);
-  const cid = await j.add({ message: "Hello World 301" });
+
   setInterval(async () => {
-    node.routing.provide(cid);
-  }, 3000);
-  console.log("Added Content:", cid.toString());
+    if (node.libp2p.getMultiaddrs().length == 0) {
+      console.log("No addresses to announce yet, skipping provide");
+      return;
+    }
+    const cid = await j.add({ message: "Hello World " + new Date() });
+    console.log("content CID: " + cid);
+    await node.routing.provide(cid);
+    await name.publish("key1", cid);
+    console.log(
+      "key: " +
+        Buffer.from((await services.keychain.exportKey("key1")).raw).toString(
+          "base64",
+        ),
+    );
+    console.log(
+      "ipns cid: " +
+        (await services.keychain.exportKey("key1")).publicKey
+          .toCID()
+          .toString(),
+    );
+  }, 10 * 1000);
 } else {
+  await sleep(1000);
   console.log("Getting content...");
   setInterval(async () => {
     // const foo = node;
     // node.libp2p.getMultiaddrs();
     // console.log(foo);
     // console.log(node.libp2p.getMultiaddrs().map((a) => a.toString()));
+    // services.lanDHT?.refreshRoutingTable();
   }, 3000);
+  // const keyRaw =
+  //   "+TrILfsyvu3b/L/toeU4SINrMi6EJSROJ8wAS4Eoc2lmFfM7DrS/AwwnYTJaBnT+C+Sxjyr7exhZoa+rCwPVTw==";
+  // services.keychain.importKey(
+  //   "key1",
+  //   privateKeyFromRaw(Buffer.from(keyRaw, "base64")),
+  // );
+
   const j = dagJson(node);
-  const cid = "baguqeerap2d52pc5kg5znbb7yocrp4keqxihhon5wgzeqmtwzcg6qkllaiaa";
-  console.log("Fetching content with CID:", cid);
-  const retrieved = await j.get(CID.parse(cid));
-  console.log("Fetched object:", retrieved);
+  setInterval(async () => {
+    const cid = CID.parse(
+      "bafzaajaiaejcb4m2eec7wlf2w2k2koe4healdpsojjb3nvcu37ns23sfmjikgktl",
+    );
+
+    console.log("resolving ipns...");
+    try {
+      const result = await name.resolve(cid as any, { nocache: true });
+      console.log(result.record);
+
+      const contentCid = result.record.value;
+      console.log("Fetching content with CID:", contentCid);
+      const retrieved = await j.get(
+        CID.parse(contentCid.substring("/ipfs/".length)),
+      );
+      console.log("Fetched object:", retrieved);
+    } catch (e) {
+      console.log("resolving failed: ", e);
+    }
+  }, 3000);
+
+  // const cid = "baguqeeraifakbvsuuoaw3hm5eumekugcgu4umkogsekwohsy6kfy6sp3s3eq";
+  // console.log("Fetching content with CID:", cid);
+  // const retrieved = await j.get(CID.parse(cid));
+  // console.log("Fetched object:", retrieved);
+
+  // name.resolve("key1");
+
   // process.exit(0);
   // node.stop();
+  while (true) {
+    await sleep(1000);
+  }
 }
